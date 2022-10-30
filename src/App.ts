af74d965d3b393d5158a4b1198aec013293cfbb5
hugolafis <77360365+hugolafis@users.noreply.github.com>
2022-10-30 17:09:28 +0000

    FPS limit

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,26 @@
 import "./App.scss";
 
 export class LifeController {
-  private something: any;
+  private canvasContext: CanvasRenderingContext2D;
+  private readonly imageData: ImageData;
 
   constructor(private readonly canvas: HTMLCanvasElement) {
+    canvas.width = 640;
+    canvas.height = 480;
+    this.canvasContext = canvas.getContext('2d');
+    this.canvasContext.imageSmoothingEnabled = false;
 
+    this.imageData = this.canvasContext.createImageData(640, 480);
+
+    this.imageData.data[0] = 0;
+    this.imageData.data[1] = 0;
+    this.imageData.data[2] = 0;
+    this.imageData.data[3] = 255;
+
+    this.canvasContext.putImageData(this.imageData, 0, 0);
   }
+
+  update = (dt: number): void => {
+    console.log('tick');
+  };
 }
--- src/Index.ts
+++ src/Index.ts
@@ -2,4 +2,26 @@ import { LifeController } from './App';
 
 const canvas = document.getElementById('#canvas') as HTMLCanvasElement;
 
-const myController = new LifeController(canvas);
+const lifeController = new LifeController(canvas);
+
+window.requestAnimationFrame(loop);
+
+let lastUpdate = performance.now();
+const targetMs = 1000;
+let dt: number = 0;
+function loop() {
+  const now = performance.now();
+  dt += now - lastUpdate;
+  lastUpdate = now;
+
+  if (dt > targetMs) {
+    // do updates
+    console.log(dt);
+
+    lifeController.update(dt);
+
+    dt = 0;
+  }
+
+  window.requestAnimationFrame(loop);
+}
